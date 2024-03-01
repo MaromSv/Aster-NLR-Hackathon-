8f084c47220ec5e2368608dfa0862864ff072f3c
dylangaliart <dylangaliart0@gmail.com>
2024-03-01 14:12:28 +0100

    better types

--- starinfo.py
+++ starinfo.py
@@ -12,43 +12,43 @@ def spectraltypeinfo(type):
     if (type == 'O'):
         color += 'blue'
         temperature += '> 30000K'
-        madeof += 'Mainly neutral and ionized helium lines and weak hydrogen lines'
+        madeof += 'Neutral and ionized helium'
     elif (type == 'B'):
         color += 'blue-white'
         temperature +=  '10000 - 30000K'
-        madeof += 'Neutral helium lines and strong hydrogen lines'
+        madeof += 'Helium and hydrogen'
     elif (type == 'A'):
         color += 'white'
         temperature += '7500 - 10000K'
-        madeof += 'Strongest hydrogen lines weak ionized calium and other metal lines'
+        madeof += 'Hydrogen'
     elif (type == 'F'):
         color += 'yellow-white'
         temperature += '6000 - 7500K'
-        madeof += 'Strong hydrogen, ionized calium and sodium lines and many lines of other ionized and neutral metals'
+        madeof += 'Hydrogen'
     elif (type == 'G'):
         color += 'yellow'
         temperature += '5200 - 6000K'
-        madeof += 'Weaker hydrogen lines, strong ionized calium and sodium lines and many lines of other ionized and neutral metals'
+        madeof += 'Ionized calium and sodium'
     elif (type == 'K'):
         color += 'orange'
         temperature += '3700 - 5200K'
-        madeof += 'Very weak hydorgen lines, strong ionized calium and sodium lines and many lines of other neutral metals'
+        madeof += 'Ionized calium and sodium'
     elif (type == 'M'):
         color += 'red'
         temperature += '2400 - 3700K'
-        madeof += 'Strong lines of neutral metals and molecular bands of titanium oxide dominate'
+        madeof += 'Neutral metals and titanium oxide dominate'
     elif (type == 'L'):
         color += 'red'
         temperature += '1300 - 2400K'
-        madeof += 'Metal hydride lines and akali metal lines'
+        madeof += 'Metal hydrides and akali metals'
     elif (type == 'T'):
         color += 'magenta'
         temperature += '700 - 1300K'
-        madeof += 'Methane lines'
+        madeof += 'Methane'
     elif (type == 'Y'):
         color += 'infrared'
         temperature += '< 700K'
-        madeof += 'Ammonia lines'
+        madeof += 'Ammonia'
     spectraltypeinfo.append(color)
     spectraltypeinfo.append(temperature)
     spectraltypeinfo.append(madeof)
@@ -64,17 +64,23 @@ def getvisibility(id):
 
 def getspecifics(id):
     info = {
-        'MWtype': 'unknown', 'spectraltype' : 'unknown', 'spectralsubtype' : 'unknown', 
-        'luminosityclass' : 'unknown', 'color' : 'unknown', 'temperature' : 'unknown',
-        'madeof' : 'unknown', 'name' : 'unknown', 'distance' : 'unknown', 'numberofstars' : '1'
+        'type': 'unknown', 'spectraltype' : 'unknown', 
+        'color' : 'unknown', 'temperature' : 'unknown',
+        'madeof' : 'unknown', 'name' : 'unknown',
+        'distance' : 'unknown', 'numberofstars' : '1'
     }
     df = get_star_data_df()
     df_grouped = df.set_index(['id'])
     spectraltypefull = df_grouped['spectral_type_full'][id]
     specs = dissectspectraltype(spectraltypefull)
-    info['MWtype'] = specs[0][0]
-    info['spectraltype'] = specs[0][1]
-    info['spectralsubtype'] = specs[0][3]
+    type = ''
+    if (specs [0][4] == ''):
+        if (specs[0][0] == ''):
+            type += 'unkown'
+        type += specs[0][0]
+    type += specs[0][4]
+    info['type'] = type
+    info['spectraltype'] = specs[0][1] + specs[0][3]
     info['luminosityclass'] = specs[0][4]
     info['color'] = specs[0][2][0]
     info['temperature'] = specs[0][2][1]
